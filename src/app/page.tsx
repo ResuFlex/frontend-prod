7de474069d4c7badd0ca7b4cacec427ff92cb4f9
ashishsingla216 <ashishsingla216@gmail.com>
2025-04-04 13:04:10 +0530

    fix: build issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Hero from "../../components/home/Hero";
 import { Features } from "../../components/home/Features";
 import { Templates } from "../../components/home/Templates";
